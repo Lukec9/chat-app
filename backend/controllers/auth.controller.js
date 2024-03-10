79fb76cbf3f6ff9a9f891a3ca22717d4689bd065
Luka Tomic <tluka234@gmail.com>
2024-03-10 08:07:10 +0100

    Initial commit
    Up to auth routes setup so far this seems amazing the guy for the tutorial is absolutely lovely and I can see that I will learn a lot of stuff and be more up to date with this

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = (req, res) => {
+  console.log("Logout user");
+};
+export const login = (req, res) => {
+  console.log("Login user");
+};
+export const logout = (req, res) => {
+  console.log("Logout user");
+};
