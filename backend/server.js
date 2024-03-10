d9bfdcafa89a9072630e6d377aa54341d6c2edfa
Luka Tomic <tluka234@gmail.com>
2024-03-10 14:31:31 +0100

    Signup endpoint and other

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js";
-const app = express();
 
-dotenv.config();
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  // root route http://localhost:5000/
-  res.send("Mjauu");
-});
+dotenv.config();
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.get("/", (req, res) => {
+//   // root route http://localhost:5000/
+//   res.send("Mjauu");
+// });
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server running on port ${PORT}`);
+});
