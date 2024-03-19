58682a5fb10bbbfb929800f24d46bef36a8a8655
Luka Tomic <tluka234@gmail.com>
2024-03-19 14:31:37 +0100

    Connected Frontend and Backend
    I had to download this from the G's repo because my stuff suddenly broke today and stopped working

--- backend/models/conversation.model.js
+++ backend/models/conversation.model.js
@@ -1,24 +1,22 @@
 import mongoose from "mongoose";
 
-const Schema = mongoose.Schema;
-
-const conversationSchema = new Schema(
-  {
-    participants: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-    messages: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Message",
-        default: [],
-      },
-    ],
-  },
-  { timestamps: true }
+const conversationSchema = new mongoose.Schema(
+	{
+		participants: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "User",
+			},
+		],
+		messages: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "Message",
+				default: [],
+			},
+		],
+	},
+	{ timestamps: true }
 );
 
 const Conversation = mongoose.model("Conversation", conversationSchema);
--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-app.use(express.json());
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -21,11 +21,11 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
 // app.get("/", (req, res) => {
-//   // root route http://localhost:5000/
-//   res.send("Mjauu");
+// root route http://localhost:5000/
+// 	res.send("Hello World!!");
 // });
 
 app.listen(PORT, () => {
-  connectToMongoDB();
-  console.log(`Server running on port ${PORT}`);
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
 });
